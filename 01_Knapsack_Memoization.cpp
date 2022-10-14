63bfbae965606cfd42d2d0967b75b4d32db61239
RAJAT KUMAR SINGH <81739533+rajatks1902@users.noreply.github.com>
2022-10-14 20:28:06 +0530

    wrote function for knapsack.

--- 01_Knapsack_Memoization.cpp
+++ 01_Knapsack_Memoization.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 int dp[1005][1005];
 int knapsack(int wt[], int val[], int W,int n){
-   
+   if(n==0 || W==0){
+        return 0;
+    }
+    if(dp[n][W]!=-1){
+        return dp[n][W];
+    }
+    if(wt[n-1]<=W){
+        return dp[n][W]=max(val[n-1]+knapsack(wt,val,W-wt[n-1],n-1),knapsack(wt,val,W,n-1));
+    }
+    else{
+        return dp[n][W]=knapsack(wt,val,W,n-1);
+    }
 }
 int main(){
     memset(dp,-1,sizeof(dp));
