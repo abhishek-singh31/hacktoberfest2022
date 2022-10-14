8815195c5d01b3766af2827fa5fe586350ce5dfc
Aman Bhavsar <96308303+5264A@users.noreply.github.com>
2022-10-14 22:33:02 +0530

    Update Subset_Sum_Memoization.cpp

--- Subset_Sum_Memoization.cpp
+++ Subset_Sum_Memoization.cpp
@@ -1,36 +1,42 @@
-#include<bits/stdc++.h>
+// A recursive solution for subset sum problem
+#include <iostream>
 using namespace std;
-int dp[105][100005];
-bool subsetSum(int arr[],int sum,int n){
-    if(sum==0){
+  
+// Returns true if there is a subset
+// of set[] with sum equal to given sum
+bool isSubsetSum(int set[], int n, int sum)
+{
+    
+    // Base Cases
+    if (sum == 0)
         return true;
-    }
-    if(n==0){
+    if (n == 0)
         return false;
-    }
-    if(dp[n][sum]!=-1)return dp[n][sum];
-    if(arr[n-1]<=sum){
-        return dp[n][sum]=subsetSum(arr,sum-arr[n-1],n-1) || subsetSum(arr,sum,n-1);
-    }
-    else{
-        return dp[n][sum]=subsetSum(arr,sum,n-1);
-    }
+  
+    // If last element is greater than sum,
+    // then ignore it
+    if (set[n - 1] > sum)
+        return isSubsetSum(set, n - 1, sum);
+  
+    /* else, check if sum can be obtained by any 
+of the following:
+      (a) including the last element
+      (b) excluding the last element   */
+    return isSubsetSum(set, n - 1, sum)
+           || isSubsetSum(set, n - 1, sum - set[n - 1]);
 }
-int main(){
-    memset(dp,-1,sizeof(dp));
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    int sum;
-    cin>>sum;
-    if(subsetSum(arr,sum,n)){
-        cout<<"Subset Present\n";
-    }
-    else{
-        cout<<"Subset Not Present\n";
-    }
+  
+// Driver code
+int main()
+{
+    int set[] = { 3, 34, 4, 12, 5, 2 };
+    int sum = 9;
+    int n = sizeof(set) / sizeof(set[0]);
+    if (isSubsetSum(set, n, sum) == true)
+         cout <<"Found a subset with given sum";
+    else
+        cout <<"No subset with given sum";
     return 0;
+}
+ 
 }
